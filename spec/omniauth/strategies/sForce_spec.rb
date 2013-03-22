6ed2abb0c5f0ecd76219698ac1e6470f03f26813
Victor Vargas <v.vargas2009@gmail.com>
2013-03-22 12:05:19 -0700

    Changes and fixes to the omniauth startegy

--- spec/omniauth/strategies/sForce_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe OmniAuth::Strategies::SForce do
-  it 'should do some testing' do
-    pending
-  end
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'rspec'
 require 'rack/test'
 require 'webmock/rspec'
 require 'omniauth'
-require 'omniauth-SForce'
+require 'omniauth-iHealth'
 
 RSpec.configure do |config|
   config.include WebMock::API
