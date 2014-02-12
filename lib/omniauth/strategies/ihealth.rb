d556b7e9383761847c7f6a261215ef9110aafb1f
Hunter Spinks <hspinks@gmail.com>
2014-02-11 17:01:49 -0800

    Adds to available API names; updates to more common OmniAuth strategy syntax

--- lib/omniauth/strategies/ihealth.rb
+++ lib/omniauth/strategies/ihealth.rb
@@ -1,30 +1,52 @@
 require 'omniauth-oauth2'
+# require 'httparty'
+# require 'awesome_print'
 
 module OmniAuth
   module Strategies
     class IHealth < OmniAuth::Strategies::OAuth2
 
-      AVAILABLE_API_NAMES = "OpenApiBP OpenApiWeight"
+      AVAILABLE_API_NAMES = "OpenApiActivity OpenApiBG OpenApiBP OpenApiSleep OpenApiSpO2 OpenApiUserInfo OpenApiWeight"
 
-      def initialize(*args)
-        super
-        options.name = "ihealth"
-        options.provider_ignores_state = true
+      option :name, 'ihealth'
+      option :provider_ignores_state, true
 
-        options.client_options = {
-            :site => options.use_sandbox ?  'http://sandboxapi.ihealthlabs.com' : 'https://api.ihealthlabs.com:8443',
-            :authorize_url => '/api/OAuthv2/userauthorization.ashx',
-            :token_url  => '/api/OAuthv2/userauthorization.ashx',
-            :token_method =>:get
-        }
+      option :client_options, {
+        :site => 'https://api.ihealthlabs.com:8443',
+        :authorize_url => '/OpenApiV2/OAuthv2/userauthorization/',
+        :token_url => '/OpenApiV2/OAuthv2/userauthorization/',
+        :token_method => :get
+      }
 
-        options.authorize_params = {
-            :APIName => AVAILABLE_API_NAMES,
-        }
+      option :authorize_params, {
+        :response_type => 'code',
+        :APIName => AVAILABLE_API_NAMES
+      }
 
+      option :token_params, {
+        :grant_type => "authorization_code"
+      }
+
+      def token_params
+        super.tap do |params|
+          params[:client_id] = options.client_id
+          params[:client_secret] = options.client_secret
+        end
       end
 
+      # def build_access_token
+      #   puts "------------------------------"
+      #   verifier = request.params['code']
+      #   uri = options.client_options.site + options.client_options.token_url
+      #   body = {:code => verifier, :redirect_uri => callback_url}.merge(token_params.to_hash(:symbolize_keys => true))
+      #   url = uri + "?" + body.to_query
+      #   puts url
+      #   response = HTTParty.get(url)
+      #   ap response
+      # end
+
       uid { raw_info['id'] }
+
     end
   end
 end
