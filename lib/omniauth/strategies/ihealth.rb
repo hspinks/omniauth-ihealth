cd7fbb18ab3f648e7f5e9148ea15ce381947904e
Hunter Spinks <hspinks@gmail.com>
2014-02-11 18:44:39 -0800

    Overrides the build_access_token method as a workaround for iHealth authentication API response formatting issue

--- lib/omniauth/strategies/ihealth.rb
+++ lib/omniauth/strategies/ihealth.rb
@@ -1,6 +1,5 @@
 require 'omniauth-oauth2'
-# require 'httparty'
-# require 'awesome_print'
+require 'oauth2'
 
 module OmniAuth
   module Strategies
@@ -15,7 +14,8 @@ module OmniAuth
         :site => 'https://api.ihealthlabs.com:8443',
         :authorize_url => '/OpenApiV2/OAuthv2/userauthorization/',
         :token_url => '/OpenApiV2/OAuthv2/userauthorization/',
-        :token_method => :get
+        :token_method => :get,
+        :parse => :json
       }
 
       option :authorize_params, {
@@ -34,16 +34,19 @@ module OmniAuth
         end
       end
 
-      # def build_access_token
-      #   puts "------------------------------"
-      #   verifier = request.params['code']
-      #   uri = options.client_options.site + options.client_options.token_url
-      #   body = {:code => verifier, :redirect_uri => callback_url}.merge(token_params.to_hash(:symbolize_keys => true))
-      #   url = uri + "?" + body.to_query
-      #   puts url
-      #   response = HTTParty.get(url)
-      #   ap response
-      # end
+      def build_access_token
+        token_url_params = {:code => request.params['code'], :redirect_uri => callback_url}.merge(token_params.to_hash(:symbolize_keys => true))
+        parsed_response = client.request(options.client_options.token_method, client.token_url(token_url_params), parse: :json).parsed
+        hash = {
+          :access_token => parsed_response["AccessToken"],
+          :expires_in => parsed_response["Expires"],
+          :refresh_token => parsed_response["RefreshToken"],
+          :user_id => parsed_response["UserId"],
+          :api_name => parsed_response["APIName"],
+          :client_para => parsed_response["client_para"]
+        }
+        OAuth2::AccessToken.from_hash(client, hash)
+      end
 
       uid { raw_info['id'] }
 
