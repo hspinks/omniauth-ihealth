04579f381db70cd11486a313bb4f86a0935198bd
Hunter Spinks <hspinks@gmail.com>
2014-02-13 13:18:46 -0800

    Formats response for OmniAuth schema

--- lib/omniauth/strategies/ihealth.rb
+++ lib/omniauth/strategies/ihealth.rb
@@ -8,6 +8,7 @@ module OmniAuth
       AVAILABLE_API_NAMES = "OpenApiActivity OpenApiBG OpenApiBP OpenApiSleep OpenApiSpO2 OpenApiUserInfo OpenApiWeight"
 
       option :name, 'ihealth'
+      option :scope, AVAILABLE_API_NAMES
       option :provider_ignores_state, true
 
       option :client_options, {
@@ -18,19 +19,18 @@ module OmniAuth
         :parse => :json
       }
 
-      option :authorize_params, {
-        :response_type => 'code',
-        :APIName => AVAILABLE_API_NAMES
-      }
-
-      option :token_params, {
-        :grant_type => "authorization_code"
-      }
+      def authorize_params
+        super.tap do |params|
+          params[:response_type] = 'code'
+          params[:APIName] = options.scope
+        end
+      end
 
       def token_params
         super.tap do |params|
-          params[:client_id] = options.client_id
-          params[:client_secret] = options.client_secret
+          params[:client_id] = client.id
+          params[:client_secret] = client.secret
+          params[:grant_type] = "authorization_code"
         end
       end
 
@@ -41,14 +41,69 @@ module OmniAuth
           :access_token => parsed_response["AccessToken"],
           :expires_in => parsed_response["Expires"],
           :refresh_token => parsed_response["RefreshToken"],
-          :user_id => parsed_response["UserId"],
+          :user_id => parsed_response["UserID"],
           :api_name => parsed_response["APIName"],
           :client_para => parsed_response["client_para"]
         }
-        OAuth2::AccessToken.from_hash(client, hash)
+        ::OAuth2::AccessToken.from_hash(client, hash)
       end
 
-      uid { raw_info['id'] }
+      uid { access_token[:user_id] }
+
+      info { user_data.slice(:name, :nickname, :image) }
+
+      extra do
+        { :user_info => user_data, :raw_info => raw_info }
+      end
+
+      def raw_info
+        access_token.options[:mode] = :query
+        user_profile_params = {:client_id => client.id, :client_secret => client.secret, :access_token => access_token.token}
+        user_profile_params.merge({:sc => options.sc, :sv => options.sv}) if options.sc && options.sv
+        @raw_info ||= access_token.get("/openapiv2/user/#{access_token[:user_id]}.json/?#{user_profile_params.to_param}", parse: :json).parsed
+      end
+
+      def user_data
+        info = raw_info
+        user_data ||= {
+          :name => info["nickname"],
+          :gender => info["gender"],
+          :birthday => Time.at(info["dateofbirth"]).to_date.strftime("%Y-%m-%d"),
+          :image => info["logo"],
+          :nickname => info["nickname"],
+          :height => calc_height(info["height"], info["HeightUnit"]),
+          :weight => calc_weight(info["weight"], info["WeightUnit"])
+        }
+      end
+
+      protected
+      CM_TO_IN_CONVERSION = 0.393701
+      FT_TO_IN_CONVERSION = 12
+      def calc_height(value, unit)
+        case(unit)
+        when 0  # value is in cm
+          return value * CM_TO_IN_CONVERSION
+        when 1  # value is in feet
+          return value * FT_TO_IN_CONVERSION
+        else    # unrecognized unit
+          return value
+        end
+      end
+
+      KG_TO_LBS_CONVERSION = 2.20462
+      STONE_TO_LBS_CONVERSION = 14
+      def calc_weight(value, unit)
+        case(unit)
+        when 0  # value is in kg
+          return value * KG_TO_LBS_CONVERSION
+        when 1  # value is in lbs
+          return value
+        when 2  # value is in stone
+          return value * STONE_TO_LBS_CONVERSION
+        else    # unrecognized unit
+          return value
+        end
+      end
 
     end
   end
